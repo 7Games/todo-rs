e2fe372e3c2f9788b8cb2436ddf2098f5586e01b
Benjamin <benjamin@sevengames.xyz>
2023-03-14 15:29:02 +0000

    Added items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod todoitem;
+
+use crate::todoitem::*;
+
+fn menu_ui() {
+    let mut todo_list: Vec<TodoItem> = Vec::new();
+
+    add_item("Buy eggs".to_string(), false, &mut todo_list);
+    add_item("Call mum".to_string(), false, &mut todo_list);
+    add_item("Instal Gentoo".to_string(), true, &mut todo_list);
+
+    delete_item(2, &mut todo_list);
+    tick_item(1, &mut todo_list);
+
+    if todo_list.len() == 0 {
+        println!("(none)");
+    } else {
+        print_list(todo_list);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    menu_ui();
 }
---
+++ src/todoitem.rs
@@ -0,0 +1,23 @@
+pub struct TodoItem {
+    name: String,
+    ticked: bool,
+}
+
+pub fn print_list(todo_list: Vec<TodoItem>) {
+    for i in 0..todo_list.len() {
+        println!(" {} [{}] {}", (i + 1), (if todo_list[i].ticked == true {"x"} else {" "}), todo_list[i].name);
+    }
+}
+
+pub fn add_item(name: String, ticked: bool, todo_list: &mut Vec<TodoItem>) {
+    let item: TodoItem = TodoItem { name: name.to_string(), ticked: ticked };
+    todo_list.push(item);
+}
+
+pub fn delete_item(index: usize, todo_list: &mut Vec<TodoItem>) {
+    todo_list.remove(index);
+}
+
+pub fn tick_item(index: usize, todo_list: &mut Vec<TodoItem>) {
+    todo_list[index].ticked = true;
+}
